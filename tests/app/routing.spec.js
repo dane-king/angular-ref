0bf3438af280d61a24a5be0b1ee52790d4f1488f
Dane King <kingd9@gmail.com>
2016-09-09 06:44:39 -0400

    added preprocessor WIP

--- tests/app/routing.spec.js
+++ tests/app/routing.spec.js
@@ -2,26 +2,30 @@
 describe('UI Routing', function() {
   var $state, $location, resolveRoute;
   beforeEach(module('app.routing'));
-  beforeEach(inject(function ($injector, $rootScope, $templateCache) {
-    $state=$injector.get('$state');
-    $location=$injector.get('$location');
-    $templateCache.put('views/test.html','');
-    resolveRoute=function(){
+  //beforeEach(module('ngTemplates'));
+  beforeEach(inject(function($injector, $rootScope, $templateCache) {
+    $state = $injector.get('$state');
+    $location = $injector.get('$location');
+
+    $templateCache.put('views/test.html', '');
+
+    resolveRoute = function() {
       $rootScope.$digest();
     };
+
   }));
   describe('Should load templates', function() {
-    it("should go to first when state go is called", function () {
+    it("should go to first when state go is called", function() {
       $state.go('first');
       resolveRoute();
       expect($state.current.url).toEqual('/first');
     });
-    it("should go to second when state go is called", function () {
+    it("should go to second when state go is called", function() {
       $state.go('second');
       resolveRoute();
       expect($state.current.url).toEqual('/second');
     });
-    it("should go to first when invalid location go is called", function () {
+    it("should go to first when invalid location go is called", function() {
       $location.url('/ddd');
       resolveRoute();
       expect($state.current.url).toEqual('/first');
